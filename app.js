ec9af2f8c491e417684fb7d7b90e18ab0f8da0ea
sharad choudhary <126180911+sharad102003@users.noreply.github.com>
2024-08-02 21:18:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Import CORS
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
